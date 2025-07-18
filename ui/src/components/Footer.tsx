e5dcb7892449c76ef9bb4e1684d3bc209b9cfc6d
lucasnevespereira <pereiraneveslucas@gmail.com>
2025-07-18 21:48:45 +0200

    ui: update footer

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,8 +1,14 @@
+import { Github } from "lucide-react"
+
 const Footer = () => {
   return (
     <footer className="py-5 text-sm">
-      <section className="container flex flex-col md:flex-row gap-5 justify-center items-center">
-        <span>© {new Date().getFullYear()} made by <a className="link" href="https://lucasnp.com" target="_blank">lucasnp</a></span>
+      <section className="container flex flex-col md:flex-row gap-5 justify-between items-center">
+        <span>© {new Date().getFullYear()}</span>
+        <a href="https://github.com/lucasnevespereira/barecms" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 link">
+          <Github size={16} />
+          <span>View on GitHub</span>
+        </a>
       </section>
     </footer>
   )
